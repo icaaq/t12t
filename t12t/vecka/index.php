f7d4f690ebce955662952d1a364157b72d295f06
Isac Lagerblad <icaaaq@gmail.com>
2011-05-06 14:50:55 +0200

    added vecka again

---
+++ t12t/vecka/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="sv">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<style type="text/css" media="screen">
+		h1{
+		color:#066EB0;
+		}
+	</style>
+
+</head>
+
+<body>
+
+  <div id="container">
+    <div id="main" role="main">
+		<h1><?php echo date("W") ?></h1>
+    </div>
+  </div>
+  <script>
+    var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID 
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
+    s.parentNode.insertBefore(g,s)}(document,"script"));
+  </script>
+
+</body>
+</html>
