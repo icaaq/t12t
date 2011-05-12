8a677435213dcc84f22b69e94b67ef769bba1b4b
Isac Lagerblad <icaaaq@gmail.com>
2011-05-12 22:38:59 +0200

    added chipsvaluta

---
+++ chipsvaluta/index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="sv">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Tillgängligare chips valuta</title>
+	<style type="text/css" media="screen">
+	html{
+	font-family:arial;
+	background:url(bg.png) no-repeat 0 0;
+	}
+	fieldset{
+	margin:78px 0 0 30px;
+	padding:0;
+	border:0;
+
+	}
+	legend + label{
+	margin:130px 0 0 10px;
+	display:inline-block;
+	}
+	span{
+	display:block;
+	}
+	label{
+	float:left;
+	}
+	input{
+	border:1px solid #666;
+	padding:10px;
+	font-size:2em;
+	}
+	button{
+	border:1px solid #666;
+	background:#eee;
+	padding:20px 5px;
+	margin:150px 0 0 10px;
+	display:inline-block;
+	}
+	p{
+	margin:10px 0 0 10px;
+	font-size:1.8em;
+	}
+	</style>
+	<script> 
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-22732942-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);})();
+	</script>
+</head>
+
+<body>
+	<fieldset>
+		<legend>Valutaomvandlare</legend>
+		<label for="amount">
+			<span>Från</span>
+			<input id="amount" placeholder="Skriv din summa här" type="number"/>
+		</label>
+		<button id="convert"> Omvandla nu</button>
+		<p id="result" aria-live="assertive"></p>
+	</fieldset>
+  	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+	<script>
+		$(function () {
+			$("#convert").click(function(){
+				if($("#amount").length>0){
+					$("#result").text("För "+$("#amount").val() + " SEK får du "+ Math.floor($("#amount").val() / 33)+" st chipspåsar");
+				}
+			});
+		});
+	</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+<meta charset="utf-8">
+<title>#t12t = tillgänglighet</title>
+<link rel="stylesheet" title="snow" href="snow.css">
+<meta name="viewport" content="width=device-width; initial-scale=1.0;">
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script> 
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-22732942-1']);
+	_gaq.push(['_trackPageview']);
+	(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);})();
+</script>
+</head>
+<body>
+<header>
+	<h1>t12t = Tillgänglighet</h1>
+</header>
+<p>Vi håller på att fixa till lite, om det hade varit 1999 så skulle ni förmodligen sett en animerad byggsnubbe</p>
+</body>
+</html>
